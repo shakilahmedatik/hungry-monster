e3efb2f117bb57661ca2a17fe6a0a206542b53d0
Shakil Ahmed Atik <shakilatik@gmail.com>
2021-02-08 22:25:38 +0600

    minor changes

--- script.js
+++ script.js
@@ -23,39 +23,44 @@ function updateUi() {
     `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchBar.value}`
   )
     .then((response) => response.json())
-    .then((data) =>
-      data.meals.forEach((meal) => {
-        const mealItem = `
-                        <div class="col details-btn" data-id = "${meal.idMeal}">
+    .then((data) => {
+      if (data.meals) {
+        data.meals.forEach((meal) => {
+          const mealItem = `
+                        <div class="col">
                             <div class="border rounded shadow">
-                                <div class="card" ">
+                                <div class="card details-btn" data-id="${meal.idMeal}">
                                     <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
-                                    <div class="card-body">
+                                    <div class="card-body" data-id="${meal.idMeal}">
                                         <p class="card-text mini-title text-center fw-bold">${meal.strMeal}</p>
                                     </div>
                                 </div>
                             </div>
                         </div>
 `;
-        mealContainer.innerHTML += mealItem;
-      })
-    );
+          mealContainer.innerHTML += mealItem;
+        });
+      } else {
+        alert("Sorry, we couldn't find any meal!");
+      }
+    });
 }
 
 //Show Details of Meal
 detailsBtn.addEventListener("click", function (e) {
-  e.preventDefault();
-  let mealItem = e.target.parentElement.parentElement.parentElement;
-  console.log(mealItem.dataset.id);
-
-  fetch(
-    `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealItem.dataset.id}`
-  )
-    .then((response) => response.json())
-    .then((data) => {
-      let detailsCard = document.querySelector(".list-card");
-      data.meals.forEach((meal) => {
-        const html = `
+  if (
+    e.target.parentElement.classList.contains("details-btn") ||
+    e.target.tagName == "P"
+  ) {
+    let mealItem = e.target.parentElement;
+    fetch(
+      `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealItem.dataset.id}`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        let detailsCard = document.querySelector(".list-card");
+        data.meals.forEach((meal) => {
+          const html = `
                     <div class="card-img">
                       <img src="${meal.strMealThumb}"
                         class="card-img-top" alt="...">
@@ -64,7 +69,7 @@ detailsBtn.addEventListener("click", function (e) {
                        <h3 class="mt-3 card-title main-title meal-name">${meal.strMeal}</h3>
                        <h5 class="mt-3 mini-title card-title">Ingredients</h5>
                     </div>
-                      <ul class="list-group list-group-flush ">
+                      <u10 class="list-group list-group-flush ">
                       <li class="list-group-item"><i class="fas fa-check-square"></i>  ${meal.strIngredient1}</li>
                       <li class="list-group-item"><i class="fas fa-check-square"></i>  ${meal.strIngredient2}</li>
                       <li class="list-group-item"><i class="fas fa-check-square"></i>  ${meal.strIngredient3}</li>
@@ -73,15 +78,19 @@ detailsBtn.addEventListener("click", function (e) {
                       <li class="list-group-item"><i class="fas fa-check-square"></i>  ${meal.strIngredient6}</li>
                       <li class="list-group-item"><i class="fas fa-check-square"></i>  ${meal.strIngredient7}</li>
                       <li class="list-group-item"><i class="fas fa-check-square"></i>  ${meal.strIngredient8}</li>
-                    </ul>
+                      <li class="list-group-item"><i class="fas fa-check-square"></i>  ${meal.strIngredient9}</li>
+                      <li class="list-group-item"><i class="fas fa-check-square"></i>  ${meal.strIngredient10}</li>
+                    </u10>
       `;
-        detailsCard.innerHTML = html;
-      });
-    });
+          detailsCard.innerHTML = html;
+        });
 
-  mealItems.hidden = true;
-  document.querySelector(".search-box").hidden = true;
-  document.querySelector(".meal-item-details").hidden = false;
+        mealItems.hidden = true;
+        document.querySelector(".search-box").hidden = true;
+        document.querySelector(".meal-item-details").hidden = false;
+      })
+      .catch((err) => console.log(""));
+  }
 });
 
 //Stopping form to submit
